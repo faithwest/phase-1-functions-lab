b01a5ab882bcbd8ea66dc728fa19fea3ec40f042
faithwest <faithchepkoech425@gmail.com>
2023-09-20 00:51:14 +0300

    trial 2

--- index.js
+++ index.js
@@ -25,27 +25,15 @@ function distanceTravelledInFeet(a,b) {
     return total;
 }
 
-function calculatesFarePrice(x, y){
-    const feet= 264;
-    const distanceFromHqInBlocks = Math.abs(x-y);
-    const distanceFromHqInFeet = distanceFromHqInBlocks *feet;
 
-    const free = 400;
-    const medium = 2000;
-    const max = 2500;
-
-    if (distanceFromHqInFeet <= free) {
-        return 0; 
-      } else if (distanceFromHqInFeet <= medium) {
-        
-        const excessFeet = distanceFromHqInFeet - free;
-        return (excessFeet * 0.02).toFixed(2); 
-      } else if (distanceFromHqInFeet <= max) {
-        return 25; 
-      } else {
-        return 'cannot travel that far'; 
-      }
-    }
-
-
+function calculatesFarePrice(x, y) {
+    const feet = 264;
+    const distanceFromHqInFeet = Math.abs(x - y) * feet;
+  
+    if (distanceFromHqInFeet <= 400) return 0;
+    if (distanceFromHqInFeet <= 2000) return (distanceFromHqInFeet * 0.02).toFixed(2);
+    if (distanceFromHqInFeet <= 2500) return 25;
+    return 'cannot travel that far';
+  }
+  
 // Code your solution in this file!
